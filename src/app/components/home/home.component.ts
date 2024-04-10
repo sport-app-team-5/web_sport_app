17ccba5336d60c5ffcb8e31a1bd3e33499df8874
JuliethQP <j.quinchia@uniandes.edu.co>
2024-04-09 20:30:39 -0500

    Fix sonar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import {
-  TranslateService
+  TranslateService,TranslateModule
 } from '@ngx-translate/core'
-import { TranslateModule } from '@ngx-translate/core'
+
 
 @Component({
   selector: 'app-home',
