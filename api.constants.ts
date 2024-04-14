62fa22918bb6eb325fe6e54552a4cd035988039e
JuliethQP <j.quinchia@uniandes.edu.co>
2024-04-14 11:44:52 -0500

    Update additional service

--- api.constants.ts
+++ api.constants.ts
@@ -1,3 +1,3 @@
 export const API_USER_BASE_URL = 'http://userba-userl-yxouxhwljzo3-1568135970.us-east-1.elb.amazonaws.com/api/v1/'
 export const API_SPORT_PLAN_BASE_URL = 'http://planba-planl-w4rfsnc2okcj-2096963002.us-east-1.elb.amazonaws.com/api/v1/'
-export const API_ADDITIONAL_sERVICE_BASE_URL='http://additi-addit-v3v7h7ihqhda-698565346.us-east-1.elb.amazonaws.com/'
+export const API_ADDITIONAL_sERVICE_BASE_URL='http://additi-addit-v3v7h7ihqhda-698565346.us-east-1.elb.amazonaws.com/api/v1'
