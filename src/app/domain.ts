0d1df184ed3dbf8a483dd9a6f9345c50def955b2
JuliethQP <j.quinchia@uniandes.edu.co>
2024-04-17 11:04:22 -0500

    Sonar update

--- src/app/domain.ts
+++ src/app/domain.ts
@@ -1,10 +1,7 @@
 import { inject } from '@angular/core'
-import { Router } from '@angular/router'
-import { tap } from 'rxjs'
 import { AuthService } from '../app/services/auth.service'
 
 export const domainGuard = () => {
-  const router = inject(Router)
   const service = inject(AuthService)
   return service.canActivate()
   
