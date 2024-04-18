450f6fafbfb21f65da7ef2dd836d1adff6212fdb
Luis Fernando santa <l.santa@uniandes.edu.co>
2024-04-18 15:26:41 -0500

    add profile menu

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common'
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { HeaderMainComponent } from '../header-main/header-main.component'
+import {DashboardComponent} from "../dashboard/dashboard.component";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
   standalone: true,
-  imports: [CommonModule, HeaderMainComponent]
+  imports: [CommonModule, HeaderMainComponent, DashboardComponent]
 })
 export class MainComponent implements OnInit {
   isOpenMenu: boolean = false
@@ -36,7 +37,21 @@ export class MainComponent implements OnInit {
     }
   }
 
-  setMenuAtive (value: any) {
+  getButtonClassesMenuProfileSvg () {
+    return {
+      cell: true,
+      'cell-active': this.isActiveMenu === 'profile'
+    }
+  }
+
+  getButtonClassesMenuProfile () {
+    return {
+      'container-home': true,
+      'active-button': this.isActiveMenu === 'profile'
+    }
+  }
+
+  setMenuActive (value: any) {
     this.isActiveMenu = value
   }
 
@@ -63,7 +78,7 @@ export class MainComponent implements OnInit {
     sessionStorage.clear()
     this.router.navigate(['/'])
   }
-  
+
   createProduct () {
     this.router.navigate(['/products'])
   }
