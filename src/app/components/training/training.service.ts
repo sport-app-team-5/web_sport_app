add82efac28566b197b38b4289f976c0c584845a
Luis Fernando santa <l.santa@uniandes.edu.co>
2024-05-03 12:42:59 -0500

    add base component

--- src/app/components/training/training.service.ts
+++ src/app/components/training/training.service.ts
@@ -23,4 +23,16 @@ export class TrainingService {
     })
     return this.http.post(API_SPORT_PLAN_BASE_URL + 'auth/trainings', data, { headers })
   }
+
+  getTrainings (): Observable<any> {
+    let token = null
+    if (typeof window !== 'undefined' && window.sessionStorage) {
+      token = sessionStorage.getItem('access_token')
+    }
+
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    })
+    return this.http.get(API_SPORT_PLAN_BASE_URL + 'auth/trainings/sportsman', { headers })
+  }
 }
