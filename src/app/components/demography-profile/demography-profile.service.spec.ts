138f3ed92f371bc696607f8f55daba8749146f19
Luis Fernando santa <l.santa@uniandes.edu.co>
2024-05-11 12:36:17 -0500

    add test to component ts

--- src/app/components/demography-profile/demography-profile.service.spec.ts
+++ src/app/components/demography-profile/demography-profile.service.spec.ts
@@ -1,16 +1,47 @@
 import { TestBed } from '@angular/core/testing';
-
-import { DemographyProfileService } from './demography-profile.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {TranslateLoader, TranslateModule, TranslateService} from "@ngx-translate/core";
+import {HttpLoaderFactory} from "../../app.config";
+import {HttpClient} from "@angular/common/http";
+import {API_SPORT_PLAN_BASE_URL} from "../../../../api.constants";
+import {DemographyProfileService} from "./demography-profile.service";
 
 describe('DemographyProfileService', () => {
-  let service: DemographyProfileService;
+  let service: DemographyProfileService
+  let httpMock: HttpTestingController
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DemographyProfileService);
-  });
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ],
+      providers: [DemographyProfileService,TranslateService]
+    })
+
+    service = TestBed.inject(DemographyProfileService)
+    httpMock = TestBed.inject(HttpTestingController)
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create nutritional information', () => {
+    const mockData = { User: 'Test User' }
+    service.getProfile({}).subscribe(data => {
+      expect(data).toEqual(mockData)
+    })
+
+    const req = httpMock.expectOne(`${API_SPORT_PLAN_BASE_URL}auth/sports_men/profile/sport`)
+    expect(req.request.method).toBe('GET')
+    req.flush(mockData)
+  })
 });
