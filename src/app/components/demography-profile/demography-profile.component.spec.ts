d479bd97e491965bc757f4d748e10867159b09be
Luis Fernando santa <l.santa@uniandes.edu.co>
2024-05-11 14:05:16 -0500

    add tests

--- src/app/components/demography-profile/demography-profile.component.spec.ts
+++ src/app/components/demography-profile/demography-profile.component.spec.ts
@@ -1,23 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { DemographyProfileComponent } from './demography-profile.component';
+import {TranslateLoader, TranslateModule, TranslateService} from "@ngx-translate/core";
+import {Observable, of} from "rxjs";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ToastrModule} from "ngx-toastr";
+import {HttpLoaderFactory} from "../../app.config";
+import {DemographyProfileComponent} from "./demography-profile.component";
+import {DemographyProfileService} from "./demography-profile.service";
 
 describe('DemographyProfileComponent', () => {
-  let component: DemographyProfileComponent;
-  let fixture: ComponentFixture<DemographyProfileComponent>;
+  let component: DemographyProfileComponent
+  let fixture: ComponentFixture<DemographyProfileComponent>
+  let demographyProfileService: DemographyProfileService
+  let translateService: TranslateService
+
+  const fakeService1 = {
+    getProfile (): Observable<any[]> {
+      return of([
+        { id: 1, name: 'Country1' },
+        { id: 2, name: 'Country2' }
+      ])
+    }
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DemographyProfileComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(DemographyProfileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      imports: [
+        DemographyProfileComponent,
+        HttpClientModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ],
+      providers: [DemographyProfileService]
+    }).compileComponents()
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DemographyProfileComponent)
+    component = fixture.componentInstance
+    demographyProfileService = TestBed.inject(DemographyProfileService)
+    translateService = TestBed.inject(TranslateService)
+    fixture.detectChanges()
+  })
+
+  describe('validate profile', () => {
+    it('should get profile info', () => {
+      let mock = TestBed.inject(DemographyProfileService)
+      spyOn(mock, 'getProfile').and.returnValue(fakeService1.getProfile())
+      component.getProfile()
+      expect(component).toBeTruthy()
+    })
+  })
+})
+
