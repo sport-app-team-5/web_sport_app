aba127a65cf6de51a27d8f22982693bbed91a892
JuliethQP <j.quinchia@uniandes.edu.co>
2024-05-18 21:56:23 -0500

    Update animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,7 +19,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 export const appConfig: ApplicationConfig = {
   providers: [
-
+    HttpClient,
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(),
