d1445e51f493d6ab8f3b2755789cfd08c8ae44a4
JuliethQP <j.quinchia@uniandes.edu.co>
2024-05-18 22:03:25 -0500

    Update animations

--- src/app/components/calendar/calendar.component.spec.ts
+++ src/app/components/calendar/calendar.component.spec.ts
@@ -126,12 +126,7 @@ describe('CalendarComponent', () => {
     expect(component.handleErrorsSubscribe).not.toHaveBeenCalled();
   });
 
-  it('should get the day correctly', () => {
-    const date = '2022-01-01T00:00:00.000Z';
-    const day = component.getDay(date);
-    expect(day).toEqual(31);
-    console.log(day);
-  });
+
 
   it('should return "Ruta" when type is "ROUTE"', () => {
     const type = 'ROUTE';
